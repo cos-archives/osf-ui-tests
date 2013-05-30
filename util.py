492a943b3dc869d1d733785c71292acdffe2e82c
Joshua Carp <jm.carp@gmail.com>
2013-05-30 11:01:17 -0400

    added user creation

--- util.py
+++ util.py
@@ -9,38 +9,75 @@ import config
 from pymongo import MongoClient
 client = MongoClient(config.mongo_uri)
 
-def login(driver, username, password):
+def fill_form(form, fields):
+    """
+    """
+    
+    # Enter field values
+    for field in fields:
+        form.find_element_by_id(field).send_keys(fields[field])
+    
+    # Click submit button
+    form.find_element_by_xpath('.//button[@type="submit"]').click()
+
+def login(driver, login_data):
     """Log in to OSF
 
     Args:
         driver : selenium.webdriver instance
-        username : OSF username
-        password : OSF password
+        login_data : dict of id -> value pairs for login form
+
+    Examples:
+        > login(driver, {'username' : 'test@test.test', 'password' : 'testtest'})
+
     """
     
     # Browse to OSF page
     driver.get(config.osf_home)
 
     # Click login button
-    login_link = driver.find_element_by_xpath('//a[@href="/account"]')
-    login_link.click()
-
-    # Get login elements
-    signin_email = driver.find_element_by_xpath('//form[@name="signin"]//input[@id="username"]')
-    signin_password = driver.find_element_by_xpath('//form[@name="signin"]//input[@id="password"]')
-    signin_submit = driver.find_element_by_xpath('//form[@name="signin"]//button[@type="submit"]')
+    driver.find_element_by_xpath('//a[@href="/account"]').click()
     
-    # Login
-    signin_email.send_keys(username)
-    signin_password.send_keys(password)
-    signin_submit.click()
+    # Get login form
+    login_form = driver.find_element_by_xpath('//form[@name="signin"]')
+    fill_form(login_form, login_data)
 
-def clear_users():
-    """Clear all users from database
+def create_user(driver, registration_data=config.registration_data):
+    """Create a new user account.
+
+    Args:
+        driver : selenium.webdriver instance
+        registration_data : dict of id -> value pairs for registration form
+
+    Examples:
+        > create_user(driver, {
+            'fullname' : 'test test',
+            'username' : 'test@test.com',
+            'username2' : 'test@test.com',
+            'password' : 'testtest',
+            'password2' : 'testtest',
+        }
 
     """
     
-    client[config.db_name]['user'].remove()
+    # Browse to account page
+    driver.get('%s/account' % (config.osf_home))
+    
+    # Find form
+    registration_form = driver.find_element_by_xpath('//form[@name="registration"]')
+    
+    # Fill out form
+    fill_form(registration_form, registration_data)
+
+def clear_user(username):
+    """Clear user from database
+
+    Args:
+        username : Username
+
+    """
+    
+    client[config.db_name]['user'].remove({'username' : username})
 
 def clear_project(title):
     """Clear project from database
