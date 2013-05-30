716da5c99da33b299dbef1066fa3caf21238ec79
Joshua Carp <jm.carp@gmail.com>
2013-05-30 11:03:26 -0400

    added fixtures for user / project creation

--- config.py
+++ config.py
@@ -12,3 +12,16 @@ db_name = 'test'
 # 
 username = 'test@test.test'
 password = 'testtest'
+
+# 
+registration_data = {
+    'fullname' : 'test test',
+    'username' : 'test@test.test',
+    'username2' : 'test@test.test',
+    'password' : 'testtest',
+    'password2' : 'testtest',
+}
+
+# Test project data
+project_title = 'test project'
+project_description = 'test project description'
