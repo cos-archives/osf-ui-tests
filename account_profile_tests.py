c18eaf153afbcd5d493fd557918b1e848f2bfdbb
Joshua Carp <jm.carp@gmail.com>
2013-05-31 13:49:48 -0400

    cleanup

--- account_profile_tests.py
+++ account_profile_tests.py
@@ -3,6 +3,8 @@ Selenium test suite for account profile manipulation and access
 Author: Harry Rybacki
 Date: 29May13
 """
+
+import time
 import unittest
 
 # Selenium imports
@@ -19,10 +21,7 @@ class AccountProfileTest(unittest.TestCase):
     def setUpClass(cls):
         
         # Start WebDriver
-        cls.driver = webdriver.Firefox()
-
-        # Wait for elements to appear
-        cls.driver.implicitly_wait(30)
+        cls.driver = util.launch_driver()
         
         # Create test user
         util.create_user(cls.driver)
@@ -33,10 +32,12 @@ class AccountProfileTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         
+        # Delete test user
+        util.clear_user()
+           
         # Close WebDriver
         cls.driver.close()
 
-    # initialize a Firefox webdriver
     def setUp(self):
 
         # Browse to profile
@@ -48,7 +49,9 @@ class AccountProfileTest(unittest.TestCase):
         matches expected test data
         """
         profile_name = self.driver.find_element_by_id('profile-fullname').text
-        self.assertTrue(config.registration_data['username'] in profile_name)
+        self.assertTrue(
+            config.registration_data['fullname'] in profile_name
+        )
 
     def test_change_name(self):
         """
@@ -56,48 +59,46 @@ class AccountProfileTest(unittest.TestCase):
         before changing it back to the original
         """
         # select and click the username edit field
-        edit_profile_name_button = self.driver.find_element_by_id(
-            'profile-fullname')
-        edit_profile_name_button.click()
+        self.driver.find_element_by_id('profile-fullname').click()
 
         # select the name field on the new popup
         edit_profile_name_field = self.driver.find_element_by_xpath(
-            '//div[@class="popover-content"]//input[@class="span2"]')
+            '//div[@class="popover-content"]//input[@class="span2"]'
+        )
 
         # delete the current name
         edit_profile_name_field.clear()
 
-        # enter the reverse user name
-        edit_profile_name_field.send_keys(config.registration_data['username'][::-1])
+        # enter the reverse name
+        edit_profile_name_field.send_keys(config.registration_data['fullname'][::-1])
 
         # find and click submit new name
-        edit_profile_name_submit_button = self.driver.find_element_by_xpath(
+        self.driver.find_element_by_xpath(
             '//div[@class="popover-content"]//button[@class="btn btn-primary"]'
-        )
-        edit_profile_name_submit_button.click()
+        ).click()
 
         # refresh page and assert change was made
         self.driver.refresh()
         profile_name = self.driver.find_element_by_id('profile-dfullname').text
-        self.assertTrue(config.registration_data['username'][::-1] in profile_name)
+        self.assertTrue(config.registration_data['fullname'][::-1] in profile_name)
 
         # return the username back to its original -- same as above
-        edit_profile_name_button = self.driver.find_element_by_id(
-            'profile-fullname')
-        edit_profile_name_button.click()
+        self.driver.find_element_by_id(
+            'profile-fullname'
+        ).click()
         edit_profile_name_field = self.driver.find_element_by_xpath(
-            '//div[@class="popover-content"]//input[@class="span2"]')
-        edit_profile_name_field.clear()
-        edit_profile_name_field.send_keys(config.registration_data['username'])
-        edit_profile_name_submit_button = self.driver.find_element_by_xpath(
-            '//div[@class="popover-content"]//button[@class="btn btn-primary"]'
+            '//div[@class="popover-content"]//input[@class="span2"]'
         )
-        edit_profile_name_submit_button.click()
+        edit_profile_name_field.clear()
+        edit_profile_name_field.send_keys(config.registration_data['fullname'])
+        self.driver.find_element_by_xpath(
+            '//div[@class="popover-content"]//button[@class="btn btn-primary"]'
+        ).click()
 
         # refresh page and assert username is back to the original
         self.driver.refresh()
-        profile_name = self.driver.find_element_by_id('profile-dfullname').text
-        self.assertTrue(config.registration_data['username'] in profile_name)
+        profile_name = self.driver.find_element_by_id('profile-fullname').text
+        self.assertTrue(config.registration_data['fullname'] in profile_name)
 
     @unittest.skip("not an implemented feature in OSF codebase")
     def test_change_location(self):
@@ -113,17 +114,19 @@ class AccountProfileTest(unittest.TestCase):
         HTTP status codes.
         """
         # find and click user's profile shortlink
-        public_profile_shortlink = self.driver.find_element_by_xpath(
-            '//div[@class="span4"]//a')
-        public_profile_shortlink.click
+        self.driver.find_element_by_xpath(
+            '//div[@class="span4"]//a'
+        ).click()
         # assert name still matches username
-        profile_name = self.driver.find_element_by_id('profile-dfullname'
+        profile_name = self.driver.find_element_by_id(
+            'profile-fullname'
         ).text
-        self.assertTrue(config.registration_data['username'] in profile_name)
+        self.assertTrue(config.registration_data['fullname'] in profile_name)
 
     @unittest.skip("not implemented")
     def test_check_public_project_updates(self):
         pass
 
+# Run tests
 if __name__ == '__main__':
     unittest.main()
