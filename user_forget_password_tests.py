2221a40718627997e00880da1760e270183a238f
pan <chennan47@github.com>
2013-07-26 12:31:49 -0700

    A new test about the forget password feature

---
+++ user_forget_password_tests.py
@@ -0,0 +1,85 @@
+"""
+
+"""
+
+import unittest
+
+# Selenium imports
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+# Project imports
+import base
+import util
+import config
+
+class UserForgetPasswordTests(base.SmokeTest):
+
+    def setUp(self):
+
+        # Call parent setUp
+        super(UserForgetPasswordTests, self).setUp()
+
+
+
+
+    def test_no_email(self):
+
+        # use forgetpassword with no email address
+        util.forget_password(self.driver, '')
+
+
+        # check for alert
+        alerts = util.get_alert_boxes(self.driver, 'Email address is required')
+        self.assertEqual(len(alerts), 1)
+
+
+    def test_too_short_email(self):
+        #forgetpassowrd with too short and wrong email
+        util.forget_password(self.driver, 'Alpha')
+
+        # check for alert
+        alertstooshort = util.get_alert_boxes(self.driver, 'Email address is too short')
+        self.assertEqual(len(alertstooshort), 1)
+        alertsinvalid = util.get_alert_boxes(self.driver, 'Email address is invalid')
+        self.assertEqual(len(alertsinvalid), 1)
+
+
+    def test_invalid_email(self):
+        #forgetpassowrd with invalid email
+        util.forget_password(self.driver, 'AlphaOmega')
+
+        # check for alert
+        alerts = util.get_alert_boxes(self.driver, 'Email address is invalid')
+        self.assertEqual(len(alerts), 1)
+
+
+    def test_not_registered_email(self):
+        #forgetpassowrd with not registered email
+        util.forget_password(self.driver, 'bad@email.addr')
+
+        # check for alert
+        alerts = util.get_alert_boxes(self.driver, 'Email bad@email.addr not found')
+        self.assertEqual(len(alerts), 1)
+
+
+    def test_registered_email(self):
+        # Create user data
+        self.user_data = util.create_user(self.driver)
+
+        #forgetpassowrd with registered email
+        util.forget_password(self.driver, self.user_data['username'])
+
+        # check for alert
+        alerts = util.get_alert_boxes(self.driver, 'Reset email sent')
+        self.assertEqual(len(alerts), 1)
+
+
+
+# Generate tests
+util.generate_tests(UserForgetPasswordTests)
+
+# Run tests
+if __name__ == '__main__':
+    unittest.main()
--- user_login_tests.py
+++ user_login_tests.py
@@ -23,11 +23,51 @@ class UserLoginTests(base.SmokeTest):
         
         # Create user data
         self.user_data = util.create_user(self.driver)
-    
+
+
+    def test_no_email_and_no_passowrd(self):
+
+        # Login with incorrect password
+        util.login(self.driver, '', '')
+
+        # Check for alert
+        alertsusername = util.get_alert_boxes(self.driver, 'Email address is required')
+        self.assertEqual(len(alertsusername), 1)
+        alertspassword = util.get_alert_boxes(self.driver, 'Password is required')
+        self.assertEqual(len(alertspassword), 1)
+
+    def test_no_email(self):
+
+        # Login with incorrect password
+        util.login(self.driver, '', 'badpassword')
+
+        # Check for alert
+        alerts = util.get_alert_boxes(self.driver, 'Email address is required')
+        self.assertEqual(len(alerts), 1)
+
+    def test_no_password(self):
+
+        # Login with incorrect password
+        util.login(self.driver, self.user_data['username'], '')
+
+        # Check for alert
+        alerts = util.get_alert_boxes(self.driver, 'Password is required')
+        self.assertEqual(len(alerts), 1)
+
+
+    def test_not_registered_email(self):
+
+        # Login with incorrect password
+        util.login(self.driver, 'bad@email.addr', self.user_data['password'])
+
+        # Check for alert
+        alerts = util.get_alert_boxes(self.driver, 'log-in failed')
+        self.assertEqual(len(alerts), 1)
+
     def test_incorrect_password(self):
         
         # Login with incorrect password
-        util.login(self.driver, 'bad@email.addr', 'wrongpass')
+        util.login(self.driver, self.user_data['username'], 'wrongpass')
         
         # Check for alert
         alerts = util.get_alert_boxes(self.driver, 'log-in failed')
