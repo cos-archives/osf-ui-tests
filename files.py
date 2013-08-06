a5e148b3871a9060229f48ec6814ac3368fae9e5
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-08-06 14:55:43 -0400

    Adds new tests for text-based files

--- files.py
+++ files.py
@@ -11,22 +11,16 @@ class FileHandlingTests(base.ProjectSmokeTest):
         super(FileHandlingTests, self).setUp()
 
         # Test file names
-        self.files = {
+        self.images = _generate_full_filepaths({
             'jpg': 'test.jpg',
             'png': 'test.png',
             'gif': 'test.gif',
-        }
+        })
 
-        # Make each filename a full path
-        for f in self.files:
-            self.files[f] = {
-                'path': os.path.join(  # append filename to this directory
-                    os.path.dirname(os.path.abspath(__file__)),
-                    'upload_files',
-                    self.files[f]),
-                'filename': self.files[f],
-            }
-
+        self.text_files = _generate_full_filepaths({
+            'txt': 'txtfile.txt',
+            'html': 'htmlfile.html',
+        })
 
     def _add_file(self, path):
         self.goto('files')
@@ -52,7 +46,7 @@ class FileHandlingTests(base.ProjectSmokeTest):
         return filename in self.get_element('div.page-header h1').text
 
     def test_add_file(self):
-        f = self.files['jpg']
+        f = self.images['jpg']
 
         self._add_file(f['path'])
 
@@ -62,22 +56,50 @@ class FileHandlingTests(base.ProjectSmokeTest):
 
     def test_embedded_image_previews(self):
 
-        for key in self.files:
-            self._add_file(self.files[key]['path'])
-            self.goto('file', self.files[key]['filename'])
+        for key in self.images:
+            self._add_file(self.images[key]['path'])
+            self.goto('file', self.images[key]['filename'])
 
+            # Get the src attribute of the image embedded
             src_filename = self.get_element(
                 '#file-container img[src*="{filename}"]'.format(
-                    filename=self.files[key]['filename']
+                    filename=self.images[key]['filename']
                 )
             ).get_attribute('src').split('/')[-1]
 
             self.assertEqual(
                 src_filename,
-                self.files[key]['filename']
+                self.images[key]['filename']
             )
 
+    def test_embedded_text_preview(self):
+        for key in self.text_files:
+            self._add_file(self.text_files[key]['path'])
+            self.goto('file', self.text_files[key]['filename'])
+            with open(self.text_files[key]['path']) as f:
+                contents = f.read()
+
+            self.assertEqual(
+                self.get_element('#file-container pre').text.strip(),
+                contents.strip(),
+            )
+
+
 util.generate_tests(FileHandlingTests)
+
+
+def _generate_full_filepaths(file_dict):
+    # Make each filename a full path
+    for f in file_dict:
+        file_dict[f] = {
+            'path': os.path.join(  # append filename to this directory
+                os.path.dirname(os.path.abspath(__file__)),
+                'upload_files',
+                file_dict[f]),
+            'filename': file_dict[f],
+        }
+
+    return file_dict
 
 if __name__ == '__main__':
     import unittest
