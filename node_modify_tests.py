2716574805906b5d4da9d01e4ddb20f284e0175d
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-09-11 17:45:46 -0400

    Refactors project renaming tests

---
+++ node_modify_tests.py
@@ -0,0 +1,64 @@
+import unittest
+
+from selenium.common.exceptions import NoSuchElementException
+
+from pages import helpers, LoginPage, ProjectPage
+
+
+class NodeModifyTests(unittest.TestCase):
+    def _test_title(self, page, can_modify=True):
+        old_title = page.title
+        new_title = "Shiny New Title"
+
+        try:
+            page.title = new_title
+        except NoSuchElementException:
+            if can_modify:
+                self.fail('Title editor not found.')
+
+        page.reload()
+
+        self.assertEqual(
+            page.title,
+            new_title if can_modify else old_title,
+        )
+
+        page.close()
+
+    def test_private_project_title_contributor(self):
+        self._test_title(
+            page=helpers.get_new_project(),
+            can_modify=True
+        )
+
+    def test_public_project_title_contributor(self):
+        page = helpers.get_new_project()
+        page.public = True
+        self._test_title(page, can_modify=True)
+
+    def test_public_project_title_non_contributor(self):
+        page = helpers.get_new_project()
+        page.public = True
+
+        _url = page.driver.current_url
+        page.close()
+
+        page = LoginPage().log_in(user=helpers.create_user())
+        page.driver.get(_url)
+
+        page = ProjectPage(driver=page.driver)
+
+        self._test_title(page, can_modify=False)
+
+    def test_public_project_title_anonymous(self):
+        page = helpers.get_new_project()
+        page.public = True
+
+        _url = page.driver.current_url
+
+        page.log_out()
+        page.driver.get(_url)
+
+        page = ProjectPage(driver=page.driver)
+
+        self._test_title(page, can_modify=False)
