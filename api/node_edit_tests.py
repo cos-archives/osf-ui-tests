c3833cbed1ad3eaceee4d67d335209560ae1437e
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-09-16 15:46:00 -0400

    Adds tests for non-contributor users attempting to change a node title via the API

--- api/node_edit_tests.py
+++ api/node_edit_tests.py
@@ -96,3 +96,76 @@ class ApiUserKeyTestCase(unittest.TestCase):
 
     def test_nested_component_title(self):
         self._test_title(helpers.get_new_nested_component())
+
+    def _test_title_non_contrib(self, page, user):
+        print user
+
+        node_id = page.id
+        parent_id = page.parent_id
+
+        edit_url = '{}/project/{}/edit'.format(
+            config.osf_home,
+            '{}/node/{}'.format(
+                parent_id,
+                node_id
+            ) if parent_id else node_id,
+        )
+
+        print edit_url
+
+        page.log_out()
+        page = LoginPage().log_in(helpers.create_user())
+
+        key = page.user_dashboard.settings.add_api_key()
+
+        r = requests.post(
+            edit_url,
+            auth=(key.key, ''),
+            data={
+                'name': 'title',
+                'value': 'Changed via API',
+            }
+        )
+
+        self.assertEqual(r.status_code, http.UNAUTHORIZED)
+        self.assertEqual(
+            json.loads(r.content).get('response'),
+            'success'
+        )
+
+        page = page.node(node_id, parent_id)
+
+        self.assertEqual(
+            page.title,
+            'Unchanged',
+        )
+
+        page.close()
+
+    @unittest.skip('fails: HTTP 200 (Redirect); expect Unauthorized')
+    def test_project_title_non_contrib(self):
+        user = helpers.create_user()
+        page = LoginPage().log_in(user).new_project('Unchanged')
+        self._test_title_non_contrib(page, user)
+
+    @unittest.skip('fails: HTTP 200 (Redirect); expect Unauthorized')
+    def test_subproject_title_non_contrib(self):
+        user = helpers.create_user()
+        page = LoginPage().log_in(user).new_project('Parent Project')
+        page = page.add_component('Unchanged', component_type='Project')
+        self._test_title_non_contrib(page, user)
+
+    @unittest.skip('fails: HTTP 200 (Redirect); expect Unauthorized')
+    def test_component_title_non_contrib(self):
+        user = helpers.create_user()
+        page = LoginPage().log_in(user).new_project('Parent Project')
+        page = page.add_component('Unchanged')
+        self._test_title_non_contrib(page, user)
+
+    @unittest.skip('fails: HTTP 200 (Redirect); expect Unauthorized')
+    def test_subproject_title_non_contrib(self):
+        user = helpers.create_user()
+        page = LoginPage().log_in(user).new_project('Parent Project')
+        page = page.add_component('Subproject', component_type='Project')
+        page = page.add_component('Unchanged')
+        self._test_title_non_contrib(page, user)
