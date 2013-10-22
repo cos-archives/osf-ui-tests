947f65d8fcdd299ba8e7d32eb7c0ebff5c3c5c49
Robert Liebowitz <rliebz@gmail.com>
2013-10-22 14:44:29 -0400

    Fixes existing add contributor tests and generalizes them to subprojects and components.

--- tests/projects/test_add_contributor.py
+++ tests/projects/test_add_contributor.py
@@ -1,9 +1,8 @@
-import unittest
-
 from nose.tools import *
 
 from pages.helpers import create_user
-from tests.fixtures import ProjectFixture
+from tests.fixtures import ProjectFixture, SubprojectFixture
+from tests.components.fixtures import ComponentOfProjectFixture, ComponentOfSubprojectFixture
 
 
 class AddContributorFixture(object):
@@ -12,25 +11,40 @@ class AddContributorFixture(object):
         super(AddContributorFixture, cls).setUpClass()
 
         cls.users.append(create_user())
-
         cls.page.add_contributor(cls.users[-1])
+        cls.page.type = 'component' if 'node' in cls.page.driver.current_url else 'project'
 
 
-class ProjectAddContributorFixture(AddContributorFixture, ProjectFixture):
-    pass
-
-
-class Project(ProjectAddContributorFixture):
+class AddContributorTests(AddContributorFixture):
     def test_contributor_added(self):
         assert_equal(2, len(self.page.contributors))
 
-    @unittest.skip('the log format on the OSF is incorrect')
+    def test_contributor_present(self):
+        assert_equal(self.users[-1].full_name, self.page.contributors[-1].full_name)
+
     def test_logged(self):
         assert_equal(
-            u'{} added {} to node {}'.format(
+            u'{} added {} to {} {}'.format(
                 self.users[0].full_name,
                 self.users[1].full_name,
+                self.page.type,
                 self.page.title,
             ),
             self.page.logs[0].text,
         )
+
+
+class ProjectTestCase(AddContributorTests, ProjectFixture):
+    pass
+
+
+class SubprojectTestCase(AddContributorTests, SubprojectFixture):
+    pass
+
+
+class ComponentOfProjectTestCase(AddContributorTests, ComponentOfProjectFixture):
+    pass
+
+
+class ComponentOfSubprojectTestCase(AddContributorTests, ComponentOfSubprojectFixture):
+    pass
