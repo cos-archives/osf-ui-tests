ae14f573a31da78625437225be03d6cf3dbbeb06
Nan Chen <nc6r@virginia.edu>
2013-11-05 06:10:54 -0500

    reorder component test fixed

--- component_access_tests.py
+++ component_access_tests.py
@@ -7,7 +7,6 @@ from base import ProjectSmokeTest
 
 class ComponentAccessCase(ProjectSmokeTest):
 
-    @unittest.skip('Fails due to Selenium troubles')
     def test_reorder_components(self):
         self.add_component('hypothesis', 'first')
         self.add_component('hypothesis', 'second')
@@ -18,10 +17,15 @@ class ComponentAccessCase(ProjectSmokeTest):
         ac = ActionChains(self.driver)
         a = self.driver.find_element_by_css_selector('#Nodes li:first-child')
         b = self.driver.find_element_by_css_selector('#Nodes li:last-child')
-        ac.drag_and_drop(a, b).perform()
+        ac.click_and_hold(a).perform()
+        a_chain = ActionChains(self.driver)
+        a_chain.move_to_element(b).perform()
+        a_chain.release(b).perform()
+
+        #ac.drag_and_drop(a, b).perform()
 
         self.driver.get(self.project_url)
         self.assertEqual(
-            self.get_element('#Nodes li:first-child').text,
+            self.get_element('#Nodes li:first-child h3 span a').text,
             'second',
         )
