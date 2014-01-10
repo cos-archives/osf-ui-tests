086d8168827c261e583254125c25032b51031fcb
Nan Chen <nc6r@virginia.edu>
2014-01-09 23:46:16 -0500

    file added

---
+++ addon/github/fixture.py
@@ -0,0 +1,16 @@
+from pages import FILES
+from pages.helpers import create_user
+from tests.fixtures import UserFixture, ProjectFixture, SubprojectFixture
+from tests.components.fixtures import ComponentFixture, ComponentOfProjectFixture, ComponentOfSubprojectFixture
+
+
+class GithubaddonFixture(ProjectFixture):
+    @classmethod
+    def setUpClass(cls):
+        super(GithubaddonFixture, cls).setUpClass()
+
+        _url  = cls.page.driver.current_url
+        cls.page.settings.addon_selection("GitHub")
+        cls.page.
+
+
